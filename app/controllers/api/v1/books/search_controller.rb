3c59acc9146e35598b39d2922c61a85e6ad023f5
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-07 11:09:27 -0700

    Add note to controller

--- app/controllers/api/v1/books/search_controller.rb
+++ app/controllers/api/v1/books/search_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::Books::SearchController < ApplicationController
 
-  def index
+  def index # GET /api/v1/book-search?location=<city>,<state>&quantity=<quantity>
     coordinates = MapQuestFacade.get_coordinates(params['location'])
     forecast = OpenWeatherFacade.get_forecast(coordinates)
-    
+
     books = BookSearchFacade.search_books(params['location'])
 
     render json: BookSerializer.index(params['location'], forecast, books[0], books[1])
