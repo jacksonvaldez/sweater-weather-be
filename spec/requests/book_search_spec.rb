639916114314b17edbe049ed29db5dedefadf68e
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-07 11:01:31 -0700

    Specs

--- spec/requests/book_search_spec.rb
+++ spec/requests/book_search_spec.rb
@@ -2,17 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Book Search Endpoint' do
 
-  it 'returns books with search query' do
+  it 'returns books with search query', :vcr do
     get "/api/v1/book-search?location=denver,co&quantity=5"
 
     books = JSON.parse(response.body, symbolize_names: true)
 
+    # binding.pry
     expect(books).to be_a(Hash)
     expect(books[:data]).to be_a(Hash)
     expect(books[:data][:attributes]).to be_a(Hash)
-    expect(books[:data][:destination]).to be_a(String)
-    expect(books[:data][:forecast]).to be_a(String)
-    expect(books[:data][:total_books_founds]).to be_a(Integer)
-    expect(books[:data][:books]).to be_a(Array)
+    expect(books[:data][:attributes][:destination]).to be_a(String)
+    expect(books[:data][:attributes][:forecast]).to be_a(String)
+    expect(books[:data][:attributes][:total_books_found]).to be_a(Integer)
+    expect(books[:data][:attributes][:books]).to be_a(Array)
   end
 end
