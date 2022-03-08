8cac2f119b2667883879a5e6eae1ab45e3c3a47a
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-08 14:27:16 -0700

    Add test for User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  context 'validations' do
+    it { should have_secure_password }
+    it { should validate_presence_of(:password) }
+    it { should validate_presence_of(:password_confirmation) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+  end
+
+  before(:each) do
+    @user = User.create(email: 'email@email.com', password: 'password123', password_confirmation: 'password123')
+  end
+
+  it 'exists' do
+    expect(@user).to be_a(User)
+  end
+
+  it 'has attributes' do
+    expect(@user.password_digest).to be_a(String)
+    expect(@user.email).to be_a(String)
+  end
+
+  it 'doesnt create user if password and password_confirmation are not the same' do
+    user_new = User.create(email: 'email@email.com', password: 'something', password_confirmation: 'something something')
+
+    expect(user_new.valid?).to eq(false)
+  end
+
+end
