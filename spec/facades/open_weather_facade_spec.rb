776b5f87d9be4612a1494014cd9680fa1d124c01
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-07 18:25:41 -0700

    Add tests for weather facade methods

--- spec/facades/open_weather_facade_spec.rb
+++ spec/facades/open_weather_facade_spec.rb
@@ -8,4 +8,24 @@ RSpec.describe OpenWeatherFacade do
     end
   end
 
+  describe 'self.get_current_weather' do
+    it 'returns a current weather object', :vcr do
+      expect(OpenWeatherFacade.get_current_weather( {lat: 20, lng: 20} )).to be_a(CurrentWeather)
+    end
+  end
+
+  describe 'self.get_daily_forecasts' do
+    it 'returns an array of daily forecast objects', :vcr do
+      expect(OpenWeatherFacade.get_daily_forecasts( {lat: 20, lng: 20} )).to be_a(Array)
+      expect(OpenWeatherFacade.get_daily_forecasts( {lat: 20, lng: 20} ).first).to be_a(DailyForecast)
+    end
+  end
+
+  describe 'self.get_hourly_forecasts' do
+    it 'returns an array of hourly forecast objects', :vcr do
+      expect(OpenWeatherFacade.get_hourly_forecasts( {lat: 20, lng: 20} )).to be_a(Array)
+      expect(OpenWeatherFacade.get_hourly_forecasts( {lat: 20, lng: 20} ).first).to be_a(HourlyForecast)
+    end
+  end
+
 end
