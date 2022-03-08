d5c1b7632eb2176bdc57b6643fdc8254ab2291b9
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-07 18:27:29 -0700

    Add forecast search route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       get 'book-search', to: 'books/search#index'
+      get 'forecast', to: 'forecast/search#index'
     end
   end
 end
