62b4b0b04384ec945d30971bd4acc226a82106cd
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-07 18:26:10 -0700

    Add methods for current weather, daily forecasts, and hourly forecasts

--- app/facades/open_weather_facade.rb
+++ app/facades/open_weather_facade.rb
@@ -1,8 +1,29 @@
 class OpenWeatherFacade
 
   def self.get_forecast(coordinates)
-    data = OpenWeatherService.get_forecast(coordinates)
-    data[:daily][0][:weather][0][:description]
+    forecast = OpenWeatherService.get_forecast(coordinates)
+    forecast[:daily][0][:weather][0][:description]
+  end
+
+  def self.get_current_weather(coordinates)
+    current_weather_data = OpenWeatherService.get_forecast(coordinates)[:current]
+    CurrentWeather.new(current_weather_data)
+  end
+
+  def self.get_daily_forecasts(coordinates)
+    daily_forecasts = OpenWeatherService.get_forecast(coordinates)[:daily]
+
+    daily_forecasts.map do |daily_forecast_data|
+      DailyForecast.new(daily_forecast_data)
+    end
+  end
+
+  def self.get_hourly_forecasts(coordinates)
+    hourly_forecasts = OpenWeatherService.get_forecast(coordinates)[:hourly]
+
+    hourly_forecasts.map do |hourly_forecast_data|
+      HourlyForecast.new(hourly_forecast_data)
+    end
   end
 
 end
