11c9beeccf6fb25339a3fa4d5a4f233cadf776d0
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-03-08 14:27:25 -0700

    Add User model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+end
